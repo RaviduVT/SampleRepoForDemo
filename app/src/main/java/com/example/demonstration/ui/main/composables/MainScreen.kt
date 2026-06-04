110ba57d86bf92cfe4daace743855f496694a10a
Ravidu Vithana <raviduv@treinetic.com>
2026-06-04 11:26:58 +0530

    did a change in the text file

--- app/src/main/java/com/example/demonstration/ui/main/composables/MainScreen.kt
+++ app/src/main/java/com/example/demonstration/ui/main/composables/MainScreen.kt
@@ -22,7 +22,7 @@ fun MainScreen(){
                 verticalArrangement = Arrangement.Top
             ) {
                 Text(
-                    text = "This is the main screen",
+                    text = "This is the main screen sdofjsodifhsdf",
                     fontSize = 32.sp
                 )
             }
